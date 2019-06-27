34fb6a091e2db2fb514abba1206ce67a9b045a55
Serge Aleynikov <saleyn@gmail.com>
2019-06-27 08:17:22 +0600

    Fix 32 bit build

--- build_port_win32.bat
+++ build_port_win32.bat
@@ -1,4 +1,5 @@
 @echo off
+set PATH=c:\Program Files (x86)\erl10.4\bin;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 
 nmake /f Makefile compile
